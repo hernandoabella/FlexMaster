0b5cc8c05bf3844fe795226c4f4d57ebd2433bb9
Hernando Abella <hernandoabella@gmail.com>
2023-07-02 22:42:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const justifyContentSelect = document.getElementById("justify-content");
-const alignItemsSelect = document.getElementById("align-items");
-const container = document.querySelector(".container");
+const flexDirectionSelect = document.getElementById('flex-direction');
+const justifyContentSelect = document.getElementById('justify-content');
+const alignItemsSelect = document.getElementById('align-items');
+const container = document.querySelector('.container');
 
-justifyContentSelect.addEventListener("change", () => {
+flexDirectionSelect.addEventListener('change', () => {
+  container.style.flexDirection = flexDirectionSelect.value;
+});
+
+justifyContentSelect.addEventListener('change', () => {
   container.style.justifyContent = justifyContentSelect.value;
 });
 
-alignItemsSelect.addEventListener("change", () => {
+alignItemsSelect.addEventListener('change', () => {
   container.style.alignItems = alignItemsSelect.value;
 });
